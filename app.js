4bc0e3c6c75708f771c11a6111deeacf772a0546
Kevin Kibebe <kibebekevin@gmail.com>
2025-03-27 17:42:29 +0300

    Updates app.js and fly.toml configurations
    Modifies app.js to listen on '0.0.0.0' in addition to the specified port
    Updates fly.toml with new app name and environment variables
    
    Fixes #N/A

--- app.js
+++ app.js
@@ -107,4 +107,4 @@ app.use((req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.listen(PORT, '0.0.0.0',() => console.log(`Server running on port: ${PORT}`));
